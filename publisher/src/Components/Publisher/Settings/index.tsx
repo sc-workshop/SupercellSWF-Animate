bb7c664daaf3a4e4f9f73accfb8cb233626a2f9b
Daniil-SV <daniilsivacenko@gmail.com>
2025-02-25 22:35:22 +0200

    fixed publisher build error

---
+++ publisher/src/Components/Publisher/Settings/index.tsx
@@ -0,0 +1,94 @@
+
+import Locale from "../../../Localization";
+import { Settings, SWFType } from "../../../PublisherSettings";
+
+import SubMenu from "../../Shared/SubMenu";
+import TextureSettings from "./textures";
+import BoolField from "../../Shared/BoolField";
+import FileField from "../../Shared/FileField";
+import { useState } from "react";
+import OtherSettings from "./others";
+import { GetPublishContext } from "../../../Context";
+
+export default function SettingsMenu() {
+    const [isExportToExternal, setExportToExternal] = useState(Settings.getParam("exportToExternal"));
+    const { toggleBackwardCompatibility, fileType } = GetPublishContext();
+
+    let is_sc1 = fileType == SWFType.SC1;
+
+    const exportToExternal = new BoolField(
+        {
+            name: Locale.Get("TID_SWF_SETTINGS_EXPORT_TO_EXTERNAL"),
+            keyName: "export_to_external_select",
+            defaultValue: Settings.getParam("exportToExternal"),
+            style: {
+                marginBottom: "10px",
+                display: "flex",
+                alignItems: "center"
+            },
+            callback: [isExportToExternal, setExportToExternal],
+            tip_tid: "TID_SWF_SETTINGS_EXPORT_TO_EXTERNAL_TIP"
+        }
+    ).render()
+    Settings.data["exportToExternal"] = isExportToExternal;
+
+    const backwardCompatibility = new BoolField(
+        {
+            name: Locale.Get("TID_SWF_SETTINGS_BACKWARD_COMPATIBILITY"),
+            keyName: "backward_compatibility_select",
+            defaultValue: Settings.getParam("backwardCompatibility"),
+            style: {
+                marginBottom: "10px",
+                display: "flex",
+                alignItems: "center"
+            },
+            callback: toggleBackwardCompatibility,
+            tip_tid: "TID_SWF_SETTINGS_BACKWARD_COMPATIBILITY_TIP"
+        }
+    ).render()
+
+    const externalFilePath = FileField(
+        Locale.Get("TID_SWF_SETTINGS_EXPORT_TO_EXTERNAL_PATH"),
+        "export_to_external_path",
+        "read",
+        "sc",
+        {
+            marginLeft: "2%",
+            marginBottom: "10px",
+            display: "flex",
+            alignItems: "center"
+        },
+        function (value) { Settings.setParam("exportToExternalPath", value) },
+        Settings.getParam("exportToExternalPath")
+    )
+
+    const repackAtlas = new BoolField(
+        {
+            name: Locale.Get("TID_SWF_REPACK_ATLAS"),
+            keyName: "repack_atlas_select",
+            defaultValue: Settings.getParam("repackAtlas"),
+            style: {
+                marginLeft: "2%",
+                marginBottom: "10px",
+                display: "flex",
+                alignItems: "center"
+            },
+            callback: value => (Settings.setParam("repackAtlas", value)),
+            tip_tid: "TID_SWF_REPACK_ATLAS_TIP"
+        }
+    ).render()
+
+    return SubMenu(
+        Locale.Get("TID_ADDITIONAL_SETTINGS_LABEL"),
+        "additional_settings",
+        {
+            marginBottom: "20%"
+        },
+        exportToExternal,
+        isExportToExternal ? externalFilePath : undefined,
+        isExportToExternal ? repackAtlas : undefined,
+        is_sc1 ? backwardCompatibility : undefined,
+        TextureSettings(),
+        OtherSettings()
+    )
+}
