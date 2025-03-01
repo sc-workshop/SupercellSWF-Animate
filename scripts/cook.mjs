f5043dd4e57f30fa785e0051c7ff780b1ddd9b0a
Daniil-SV <daniilsivacenko@gmail.com>
2025-03-01 14:13:50 +0200

    added installing of global packages to cook script

--- scripts/cook.mjs
+++ scripts/cook.mjs
@@ -7,8 +7,14 @@ const publisher_cwd = join(cwd(), "publisher");
 const plugin_cwd = join(cwd(), "plugin");
 
 const install_paths = [installer_cwd, publisher_cwd, plugin_cwd];
+const globals = ["ts-node", "@types/node"]
 
 for (const path of install_paths)
 {
     execSync("npm install --save-dev", { stdio: [0, 1, 2], cwd: path })
+}
+
+for (const name of globals)
+{
+    execSync(`npm install -g ${name}`, { stdio: [0, 1, 2] })
 }
